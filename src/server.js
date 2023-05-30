7284b5a271c8cb7bec6e06043f1b4da89cc391f6
christopher <chris.hend012@gmail.com>
2023-05-30 08:16:34 -0300

    Server port

--- src/server.js
+++ src/server.js
@@ -36,5 +36,5 @@ app.use((error, request, response, next) => {
 	})
 })
 
-const PORT = process.env.SERVER_PORT || 3333
+const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
